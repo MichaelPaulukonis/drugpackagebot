de79f897e3df5c8174ce08512d7b3e24c7457fed
MichaelPaulukonis <xraysmalevich@gmail.com>
2015-04-29 13:26:21 -0400

    working on pgSQL integration

--- config.js
+++ config.js
@@ -8,7 +8,8 @@ module.exports = {
     access_token:         process.env.ACCESS_TOKEN,
     access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
 
-    // static_lib:           (process.env.STATIC_LIB.toLowerCase() === 'true'),
+    database_url:         process.env.DATABASE_URL,
+
     tweet_on:             (process.env.TWEET_ON.toLowerCase() === 'true'),
     log:                  (process.env.LOG.toLowerCase() === 'true'),
     seconds:              parseInt(process.env.SECONDS, 10),
--- sequencer.js
+++ sequencer.js
@@ -1,9 +1,39 @@
+// pg-code based on https://github.com/Gangles/every-game-bot/blob/master/everygamebot.js
+var pg = require('pg');
+var config = require('./config.js');
 
+var DB_CREATE = 'CREATE TABLE IF NOT EXISTS sequence '
+      + '(currentIndex integer NOT NULL)';
+var DB_QUERY = 'SELECT currentIndex FROM sequence';
+var DB_INSERT = 'UPDATE sequence SET currentIndex = $1';
 
 var sequencer = function() {
 
   var list = require('./packages.txt');
   var index = 0;
+  var client = new pg.Client(config.database_url);
+
+  var initDB = function() {
+    console.log('initializing DB');
+    console.log(client);
+    try {
+      // connect to postgres db
+      client.connect(function(err) {
+	// connected, make sure table exists
+	if (err) return console.log('DB init error:', err);
+	client.query(DB_CREATE, function(err) {
+	  // table exists, start tweeting
+	  if (err) return console.log('DB init error:', err);
+	  console.log("Database initialized.");
+	  // waitToBegin();
+	});
+      });
+    } catch (e) {
+      console.log("DB init error:", e.toString());
+    }
+  };
+
+
   // console.log('length: ' + list.length + ' ' + list[0]);
 
   // TODO: need to have this be a node.js module
@@ -14,6 +44,8 @@ var sequencer = function() {
   this.next = function() {
     return list[index++];
   };
+
+  initDB();
 
 };
 
