5418e9d98cbd35e060a1a2e6d870ba0f3761671b
Michael Paulukonis <xraysmalevich@gmail.com>
2015-05-05 22:37:44 -0400

    sequencer work

--- sequencer.js
+++ sequencer.js
@@ -1,14 +1,17 @@
 // pg-code based on https://github.com/Gangles/every-game-bot/blob/master/everygamebot.js
 var pg = require('pg');
+var query = require('pg-query');
 var config = require('./config.js');
 var _ = require('underscore');
 _.mixin(require('underscore.deferred'));
 
 
+// NOTE: this does not initialize the table with the current index
+// which is.... an issue.
 var DB_CREATE = 'CREATE TABLE IF NOT EXISTS sequence '
       + '(currentIndex integer NOT NULL)';
 var DB_QUERY = 'SELECT currentIndex FROM sequence';
-var DB_INSERT = 'UPDATE sequence SET currentIndex = $1';
+var DB_UPDATE = 'UPDATE sequence SET currentIndex = $1';
 
 var sequencer = function() {
 
@@ -25,7 +28,7 @@ var sequencer = function() {
 	if (err) return console.log('DB init error:', err);
 	client.query(DB_CREATE, function(err) {
 	  // table exists, start tweeting
-	  if (err) return console.log('DB init error:', err);
+	  if (err) return console.log('DB init error (create):', err);
 	  console.log("Database initialized.");
 	  // waitToBegin();
 	});
@@ -37,21 +40,54 @@ var sequencer = function() {
 
 
   // console.log('length: ' + list.length + ' ' + list[0]);
-
-  // TODO: need to split the lines that are longer than 140 chars
-  // TODO: the index-storage needs to be external
-
-  this.next = function() {
+ this.next = function() {
     var dfd = new _.Deferred();
-    // return list[index++];
-    var sentence = list[index++];
-    // console.log(sentence);
-    dfd.resolve(sentence);
+
+    // var DB_QUERY = 'SELECT currentIndex FROM sequence';
+    // var DB_UPDATE = 'UPDATE sequence SET currentIndex = $1';
+
+   client.query(DB_QUERY, {}, function(data) {
+     // { [error: relation "sequence" does not exist]
+     //   name: 'error',
+     //   length: 99,
+     //   severity: 'ERROR',
+     //   code: '42P01',
+     //   detail: undefined,
+     //   hint: undefined,
+     //   position: '26',
+     //   internalPosition: undefined,
+     //   internalQuery: undefined,
+     //   where: undefined,
+     //   schema: undefined,
+     //   table: undefined,
+     //   column: undefined,
+     //   dataType: undefined,
+     //   constraint: undefined,
+     //   file: 'parse_relation.c',
+     //   line: '986',
+     //   routine: 'parserOpenTable' }
+     console.log(data);
+     dfd.resolve(data);
+   });
+
+    // var query = client.query(DB_QUERY);
+    // query.on('row', function(row, result) {
+
+    //   console.log(row);
+    //   console.log(result);
+    //   // return list[index++];
+    //   var sentence = list[index++];
+    //   // console.log(sentence);
+    //   dfd.resolve(sentence);
+    // });
+
+
     return dfd.promise();
+
   };
 
   // tempt turn-off
-  // initDB();
+  initDB();
 
 };
 
